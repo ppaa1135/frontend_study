fe043cf365f732492b0dc1bab04a827d315da961
Hyeonwoo Park <ppaa1135@naver.com>
2024-06-22 14:52:31 +0900

    6.2 useEffect 변수의 변화 감지하기

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,15 +2,23 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time.");
+  const onChange = (event) => setKeyword(event.target.value);
 
   useEffect(() => {
-    console.log("Call the api...");
+    console.log("I run only once");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
 
   return (
     <div>
+      <input onChange={onChange} type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
